1df4d956a8010a963a89015bbb41eb84c37c5e20
Theresa Thaller <theresa.thaller@dreipol.ch>
2024-03-27 14:48:20 +0100

    Create intro component

--- src/app/online-beratung/page.tsx
+++ src/app/online-beratung/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+import { Intro } from "@/components/common/intro/intro";
+import { useTranslation } from "../../../i18n";
+
 export default function OnlineConsulting() {
+
+  const { t } = useTranslation();
+
   return (
-    <main>Online-Beratung</main>
+    <main>
+      <Intro title={t("online_consulting.intro.title")} lead={t("online_consulting.intro.lead")} variant="light" />
+    </main>
   );
 }
