aed92ec24d691b84bdbd4d873eee30683ce67493
Theresa Thaller <theresa.thaller@dreipol.ch>
2024-05-03 15:59:19 +0200

    Add FAQ translations and create accordion templates

--- src/components/common/accordion/accordion.tsx
+++ src/components/common/accordion/accordion.tsx
@@ -1,16 +1,25 @@
-import { Accordion as MuiAccordion } from '@mui/material';
+import { AccordionSummary, Accordion as MuiAccordion } from '@mui/material';
+import { Icon } from '../icon/icon';
 import "./accordion.scss";
 
 interface AccordionProps {
+  summary: string;
   children: React.ReactNode;
 }
 
-export const Accordion = ({ children }: AccordionProps) => {
+export const Accordion = ({ summary, children }: AccordionProps) => {
 
   return (
     <div className="accordion">
       {children &&
         <MuiAccordion>
+          <AccordionSummary
+            expandIcon={<Icon icon='chevron-down' color="red" />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            {summary}
+          </AccordionSummary>
           {children}
         </MuiAccordion>}
     </div>
--- src/templates/heating-recommandations/heating-recommandations.tsx
+++ src/templates/heating-recommandations/heating-recommandations.tsx
@@ -1,8 +1,7 @@
 import { Accordion } from "@/components/common/accordion/accordion";
-import { Icon } from "@/components/common/icon/icon";
 import { FullWidth } from "@/components/common/layout/full-width/full-width";
 import { HeatingTiles } from "@/components/features/heating-tiles/heating-tiles";
-import { AccordionDetails, AccordionSummary } from "@mui/material";
+import { AccordionDetails } from "@mui/material";
 import { useTranslation } from "../../../i18n";
 
 export const HeatingRecommendations = () => {
@@ -12,14 +11,7 @@ export const HeatingRecommendations = () => {
   return (
     <FullWidth>
       <HeatingTiles />
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<Icon icon='chevron-down' color="red" />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          {t("my_property.heating_recommendations.accordion_summary")}
-        </AccordionSummary>
+      <Accordion summary={t("my_property.heating_recommendations.accordion_summary")}>
         <AccordionDetails>
           {t("my_property.heating_recommendations.accordion_detail_1")} <a href={t("my_property.heating_recommendations.accordion_link_target_1")} target="_blank">{t("my_property.heating_recommendations.accordion_link_1")}</a> {t("my_property.heating_recommendations.accordion_detail_2")}
         </AccordionDetails>
