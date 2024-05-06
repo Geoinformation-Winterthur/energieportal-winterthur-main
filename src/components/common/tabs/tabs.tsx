30dbe315f21c1eee98b8bd937aed997032424378
Theresa Thaller <theresa.thaller@dreipol.ch>
2024-05-06 16:43:06 +0200

    Add Focus Event definition

--- src/components/common/tabs/tabs.tsx
+++ src/components/common/tabs/tabs.tsx
@@ -4,54 +4,54 @@ import { KeyboardEvent, ReactNode, useState } from "react";
 import { TabsContext } from "../../../context/tabs-context";
 
 interface TabsProps {
-    children: ReactNode;
-    initialValue: string;
-    name: string;
+  children: ReactNode;
+  initialValue: string;
+  name: string;
 }
 
 export default function Tabs({ children, initialValue, name }: TabsProps) {
-    const changeTabsValue = (value: string) => {
-        setTabsValue({value, name, changeTabsValue, handleKeyDown, handleBlur})
-    }
-    const handleKeyDown = (event: KeyboardEvent) => {
-        if (event.target instanceof HTMLElement) {
-            if (event.key === 'ArrowRight') {
-                event.target.setAttribute('tabindex', '-1');
+  const changeTabsValue = (value: string) => {
+    setTabsValue({ value, name, changeTabsValue, handleKeyDown, handleBlur })
+  }
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.target instanceof HTMLElement) {
+      if (event.key === 'ArrowRight') {
+        event.target.setAttribute('tabindex', '-1');
 
-                if (event.target.nextSibling instanceof HTMLElement) {
-                    event.target.nextSibling.setAttribute('tabindex', '0');
-                    event.target.nextSibling.focus();
-                } else if (event.target.parentNode?.firstChild instanceof HTMLElement ) {
-                    event.target.parentNode.firstChild.setAttribute('tabindex', '0');
-                    event.target.parentNode.firstChild.focus();
-                }
-            }
-
-            if (event.key === 'ArrowLeft') {
-                event.target.setAttribute('tabindex', '-1');
-
-                if (event.target.previousSibling instanceof HTMLElement) {
-                    event.target.previousSibling.setAttribute('tabindex', '0');
-                    event.target.previousSibling.focus();
-                } else if (event.target.parentNode?.lastChild instanceof HTMLElement ) {
-                    event.target.parentNode.lastChild.setAttribute('tabindex', '0');
-                    event.target.parentNode.lastChild.focus();
-                }
-            }
+        if (event.target.nextSibling instanceof HTMLElement) {
+          event.target.nextSibling.setAttribute('tabindex', '0');
+          event.target.nextSibling.focus();
+        } else if (event.target.parentNode?.firstChild instanceof HTMLElement) {
+          event.target.parentNode.firstChild.setAttribute('tabindex', '0');
+          event.target.parentNode.firstChild.focus();
         }
-    }
+      }
 
-    const handleBlur = (event: FocusEvent) => {
-        if (event.target instanceof Element && !(event.target.parentNode?.contains(event.relatedTarget as Node))) {
-            event.target.setAttribute('tabindex', '-1');
-            event.target.parentNode?.querySelector('[aria-selected="true"]')?.setAttribute('tabindex', '0');
+      if (event.key === 'ArrowLeft') {
+        event.target.setAttribute('tabindex', '-1');
+
+        if (event.target.previousSibling instanceof HTMLElement) {
+          event.target.previousSibling.setAttribute('tabindex', '0');
+          event.target.previousSibling.focus();
+        } else if (event.target.parentNode?.lastChild instanceof HTMLElement) {
+          event.target.parentNode.lastChild.setAttribute('tabindex', '0');
+          event.target.parentNode.lastChild.focus();
         }
+      }
     }
-    const [tabsValue, setTabsValue] = useState({value: initialValue, name, changeTabsValue, handleKeyDown, handleBlur})
+  }
 
-    return (
-        <TabsContext.Provider value={tabsValue}>
-            {children}
-        </TabsContext.Provider>
-    )
+  const handleBlur = (event: React.FocusEvent<HTMLButtonElement>) => {
+    if (event.target instanceof Element && !(event.target.parentNode?.contains(event.relatedTarget as Node))) {
+      event.target.setAttribute('tabindex', '-1');
+      event.target.parentNode?.querySelector('[aria-selected="true"]')?.setAttribute('tabindex', '0');
+    }
+  }
+  const [tabsValue, setTabsValue] = useState({ value: initialValue, name, changeTabsValue, handleKeyDown, handleBlur })
+
+  return (
+    <TabsContext.Provider value={tabsValue}>
+      {children}
+    </TabsContext.Provider>
+  )
 }
--- src/context/tabs-context.ts
+++ src/context/tabs-context.ts
@@ -1,17 +1,17 @@
 import { KeyboardEvent, createContext } from "react";
 
 interface TabsContextType {
-    value: string;
-    name: string;
-    changeTabsValue: (value: string) => void;
-    handleKeyDown: (event: KeyboardEvent) => void;
-    handleBlur: (event: FocusEvent) => void;
+  value: string;
+  name: string;
+  changeTabsValue: (value: string) => void;
+  handleKeyDown: (event: KeyboardEvent) => void;
+  handleBlur: (event: React.FocusEvent<HTMLButtonElement>) => void;
 }
 
 export const TabsContext = createContext<TabsContextType>({
-    value: '0',
-    name: 'tabs',
-    changeTabsValue: () => undefined,
-    handleKeyDown: () => undefined,
-    handleBlur: () => undefined,
+  value: '0',
+  name: 'tabs',
+  changeTabsValue: () => undefined,
+  handleKeyDown: () => undefined,
+  handleBlur: () => undefined,
 });
