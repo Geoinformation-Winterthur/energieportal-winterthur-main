e1721c5a86870edd60b3eff1db2966a3540d0821
Theresa Thaller <theresa.thaller@dreipol.ch>
2024-11-10 23:07:42 +0100

    Update assets

--- src/components/features/teaser-tiles/teaser-tile.tsx
+++ src/components/features/teaser-tiles/teaser-tile.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/common/button/button";
-import Overlay from "@/components/common/overlay/overlay";
-import { Detail, Teaser } from "@/types/case-study";
-import Image from "next/image";
-import { DetailView } from "../detail-view/detail-view";
-import styles from "./teaser-tile.module.scss";
+import { Button } from '@/components/common/button/button';
+import Overlay from '@/components/common/overlay/overlay';
+import { Detail, Teaser } from '@/types/case-study';
+import Image from 'next/image';
+import { DetailView } from '../detail-view/detail-view';
+import styles from './teaser-tile.module.scss';
 
 interface TeaserTileProps {
   teaser: Teaser;
@@ -20,21 +20,23 @@ export const TeaserTile = ({ teaser, detail }: TeaserTileProps) => {
   );
 
   return (
-    <div className={styles["teaser-tile"]}>
-      <div className={styles["teaser-tile__content"]}>
-        <h4 className={styles["teaser-tile__title"]}>{teaser.title}</h4>
-        <div className={styles["teaser-tile__img"]}>
+    <div className={styles['teaser-tile']}>
+      <div className={styles['teaser-tile__content']}>
+        <div className={styles['teaser-tile__header']}>
+          <h4 className={styles['teaser-tile__title']}>{teaser.title}</h4>
+        </div>
+        <div className={styles['teaser-tile__img']}>
           <Image src={imagePath + teaser.imageSrc} alt={teaser.imageAlt} fill />
         </div>
-        <div className={styles["teaser-tile__section"]}>
-          <h5 className={styles["teaser-tile__subtitle"]}>{teaser.subtitle}</h5>
+        <div className={styles['teaser-tile__section']}>
+          <h5 className={styles['teaser-tile__subtitle']}>{teaser.subtitle}</h5>
           {teaser.tag && (
-            <p className={styles["teaser-tile__tag"]}>{teaser.tag}</p>
+            <p className={styles['teaser-tile__tag']}>{teaser.tag}</p>
           )}
         </div>
-        <ul className={styles["teaser-tile__list"]}>
+        <ul className={styles['teaser-tile__list']}>
           {teaser.facts.map((fact, index) => (
-            <li className={styles["teaser-tile__list-item"]} key={index}>
+            <li className={styles['teaser-tile__list-item']} key={index}>
               {fact}
             </li>
           ))}
